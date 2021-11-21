e150477d1c4abb5ca2b966955d75ae786ff06cf8
Martin Hummel <martin.hummel@hs-furtwangen.de>
2021-11-21 22:17:51 +0100

    now it should be done

---
+++ app/src/main/java/com/example/framenttest_2/Entitätsklassen/ExterneSchnittstellen/SelectFottbildung_s.java
@@ -0,0 +1,19 @@
+package com.example.framenttest_2.Entitätsklassen.ExterneSchnittstellen;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.framenttest_2.R;
+
+public class SelectFottbildung_s extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.selectfortbildung_s, container, false);
+    }
+}
